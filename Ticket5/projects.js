04bc28aac700d23e21700ece85493a511dfc8d90
Santhi Kaliyappan <ksanthi88@gmail.com>
2025-02-20 09:28:16 -0600

    added

--- Ticket5/projects.js
+++ Ticket5/projects.js
@@ -5,5 +5,17 @@ let projects = [
 ];
 let project1=projects.map(project=>project.projectName);
 console.log(project1);
-let project2=projects.filter(project=>project.status==="in progress");
-console.log(project2);
+let projects1 = [
+    { projectName: 'Project A', deadline: '2024-02-01', status: 'in progress' },
+    { projectName: 'Project B', deadline: '2024-01-15', status: 'not started' },
+    { projectName: 'Project C', deadline: '2024-03-01', status: 'in progress' }
+];
+let project2=projects1.filter(project=>project.status === 'in progress');
+console.table(project2);
+let projects3 = [
+    { projectName: 'Project A', deadline: '2024-02-01', status: 'in progress' },
+    { projectName: 'Project B', deadline: '2024-01-15', status: 'not started' },
+    { projectName: 'Project C', deadline: '2024-03-01', status: 'in progress' }
+];
+projects3.sort((a,b) => new Date(a.deadline) - new Date(b.deadline));
+console.table(projects3);
